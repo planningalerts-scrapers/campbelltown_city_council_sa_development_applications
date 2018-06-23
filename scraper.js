2a4216379c700fcbfdb34cd3a68c9932bdae932e
MichaelBone <37421067+MichaelBone@users.noreply.github.com>
2018-06-23 13:53:36 +0930

    Added quoting for column and table names.

--- scraper.js
+++ scraper.js
@@ -5,29 +5,33 @@ let pdf2json = require("pdf2json");
 
 function initDatabase(callback) {
     // Set up sqlite database.
+    
     let db = new sqlite3.Database("data.sqlite");
     db.serialize(() => {
-        db.run("create table if not exists data (name text)");
+        db.run("create table if not exists [data] ([name] text)");
         callback(db);
     });
 }
 
 function updateRow(db, value) {
     // Insert some data.
-    let statement = db.prepare("insert into data values (?)");
+    
+    let statement = db.prepare("insert into [data] values (?)");
     statement.run(value);
     statement.finalize();
 }
 
 function readRows(db) {
     // Read some data.
-    db.each("select rowid as id, name from data", (err, row) => {
+    
+    db.each("select [rowid] as [id], [name] from [data]", (err, row) => {
         console.log(row.id + ": " + row.name);
     });
 }
 
 function fetchPage(url, callback) {
     // Use request to read in pages.
+    
     request(url, (error, response, body) => {
         if (error) {
             console.log("Error requesting page: " + error);
@@ -39,8 +43,10 @@ function fetchPage(url, callback) {
 
 function run(db) {
     // Read the lodged applications page.
+    
     fetchPage("https://www.campbelltown.sa.gov.au/page.aspx?u=1973", body => {
         // Use cheerio to find things in the page with css selectors.
+        
         let $ = cheerio.load(body);
         let elements = $("div.uContentList a.href").each(() => {
             let pdfUrl = $(this).text().trim();
@@ -59,8 +65,10 @@ function run(db) {
     });
 
     // Read the approved applications page.
+        
     fetchPage("https://http://www.campbelltown.sa.gov.au/page.aspx?u=1777", body => {
         // Use cheerio to find things in the page with css selectors.
+        
         let $ = cheerio.load(body);
         let elements = $("div.uContentList a.href").each(() => {
             let value = $(this).text().trim();

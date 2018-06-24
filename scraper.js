ade9b8b92ddc789a27578ece685730286489e0b2
MichaelBone <Michael.Bone@senet.com.au>
2018-06-24 17:25:46 +0930

    Refined application number parsing.

--- scraper.js
+++ scraper.js
@@ -6,7 +6,6 @@ let urlparser = require("url");
 let moment = require("moment");
 
 const LodgedApplicationsUrl = "https://www.campbelltown.sa.gov.au/page.aspx?u=1973";
-const ApprovedApplicationsUrl = "https://www.campbelltown.sa.gov.au/page.aspx?u=1777";
 
 // Sets up an sqlite database.
 
@@ -20,7 +19,7 @@ function initializeDatabase(callback) {
 
 // Inserts a row in the database if it does not already exist.
 
-function insertRow(developmentApplication) {
+function insertRow(database, developmentApplication) {
     let sqlStatement = database.prepare("insert or ignore into [data] values (?, ?, ?, ?, ?, ?, ?, ?, ?)");
     sqlStatement.run([
         developmentApplication.applicationNumber,
@@ -70,6 +69,9 @@ function parsePdfs(database, url) {
         // Read and parse each PDF, extracting the development application text.
 
         for (let pdfUrl of pdfUrls) {
+/////////////// TESTING            
+if (!pdfUrl.includes("Lodged%20-%20November%202017"))
+    continue;
             let pdfParser = new pdf2json();
             let pdfPipe = request({ url: pdfUrl, encoding: null }).pipe(pdfParser);
             pdfPipe.on("pdfParser_dataError", error => console.error(error))
@@ -83,63 +85,116 @@ function parsePdfs(database, url) {
                 let applicationNumber = null;
                 let address = null;
                 let reason = null;
-                let informationUrl = null;
-                let commentUrl = null;
+                let informationUrl = pdfUrl;
+                let commentUrl = parsedUrl.origin;
                 let scrapeDate = moment().format("YYYY-MM-DD");
                 let lodgementDate = null;
 
+                let previousRow = null;
                 for (let row of pdfRows) {
                     // If there are two forward slashes within the first 20 characters then it is
                     // very likely an application number (and it is not formatted as a date such
                     // as "31/12/2008").  For example, "162/0082/12".
-
-                    if (row[0].trim().substring(0, 20).replace(/[^\/]/g, "").length === 2 && !moment(row[0].trim().substring(0, 10), "DD/MM/YYYY", true).isValid()) {
+// console.log(row);
+                    let parsedApplicationNumber = parseApplicationNumber(row);
+                    if (parsedApplicationNumber !== null) {
                         // Extract the development application number and lodgement date.
 
-                        applicationNumber = row[0].trim();
+                        applicationNumber = parsedApplicationNumber;
                         address = null;
                         reason = null;
-                        lodgementDate = moment(row[2].trim(), "D/MM/YYYY", true);  // allow the leading zero of the day to be omitted
+                        lodgementDate = (row.length >= 3) ? moment(row[2].trim(), "D/MM/YYYY", true) : null;  // allows the leading zero of the day to be omitted
                         haveApplicationNumber = true;
                         haveAddress = false;
-                    } else if (haveApplicationNumber) {
-                        // Extract the address of th development application.
+                    } else if (haveApplicationNumber && !haveAddress) {
+                        // Extract the address of the development application.
 
-                        address = row.join("").trim();
-                        haveApplicationNumber = false;
-                        haveAddress = true;
-                    } else if (haveAddress) {
+                        if (previousRow !== null && previousRow.join("").replace(/\s/g, "").toLowerCase().startsWith("propertyaddress")) {
+                            address = row.join("").trim();
+                            haveApplicationNumber = true;
+                            haveAddress = true;
+                        }
+                    } else if (haveApplicationNumber && haveAddress) {
                         // Extract the reason for the development application.  This is assumed
                         // to be the end of the information for this development application.
 
-                        reason = row.join("").trim();
-                        haveApplicationNumber = false;
-                        haveAddress = false;
-                        developmentApplications.push({
-                            applicationNumber: applicationNumber,
-                            address: address,
-                            reason: reason,
-                            informationUrl: informationUrl,
-                            commentUrl: commentUrl,
-                            scrapeDate: scrapeDate,
-                            lodgementDate: (lodgementDate.isValid() ? lodgementDate.format("YYYY-MM-DD") : null) });
+                        if (previousRow !== null && previousRow.join("").replace(/\s/g, "").toLowerCase().startsWith("natureofdevelopment")) {
+                            reason = row.join("").trim();
+                            haveApplicationNumber = false;
+                            haveAddress = false;
+                            developmentApplications.push({
+                                applicationNumber: applicationNumber,
+                                address: address,
+                                reason: reason,
+                                informationUrl: informationUrl,
+                                commentUrl: commentUrl,
+                                scrapeDate: scrapeDate,
+                                lodgementDate: ((lodgementDate !== null && lodgementDate.isValid()) ? lodgementDate.format("YYYY-MM-DD") : null) });
+                        }
                     }
+                    previousRow = row;
                 }
 
-                for (let developmentApplication of developmentApplications)
-                    insertRow(developmentApplication)
+                for (let developmentApplication of developmentApplications) {
                     console.log(developmentApplication);
+                    insertRow(database, developmentApplication)
+                }
             });
         }
-
-        readRows(database);
 
         // database.close();
     });
 }
 
-// Convert a parsed PDF into an array of rows.  This is based on pdf2table by Sam Decrock.
+// Parses an application number from the specified PDF row of text.
+
+function parseApplicationNumber(row) {
+    // If there are two forward slashes in the first element of the row and it is not a date then
+    // assume that the first element is an application number.  For example, "170/1318/14"
+
+    let text = row[0].trim();
+    if (text.length >= 6 && text.length <= 16 && row[0].replace(/[^\/]/g, "").length === 2 && !moment(text.substring(0, 10), "DD/MM/YYYY", true).isValid())
+        return text;
+
+    // If there is one slash in the first element and one slash in the second element and together
+    // they are not a date then assume the application number is split across two elements of the
+    // row.  For example, "170/1" and "318/14".
+
+    if (row.length < 2)
+        return null;
+    text = row[0].trim() + row[1].trim();
+    if (text.length >= 6 && text.length <= 16 && row[0].replace(/[^\/]/g, "").length === 1 && row[1].replace(/[^\/]/g, "").length === 1 && !moment(text.substring(0, 10), "DD/MM/YYYY", true).isValid())
+        return text;
+
+    // Assume that an application number is not present.
+
+    return null;
+}
+
+// Convert a parsed PDF into an array of rows.  This function is based on pdf2table by Sam Decrock.
 // See https://github.com/SamDecrock/pdf2table/blob/master/lib/pdf2table.js.
+//
+// Copyright (c) 2015 Sam Decrock <sam.decrock@gmail.com>
+//
+// MIT License
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 function convertPdfToText(pdf) {
     let comparer = (a, b) => (a.x > b.x) ? 1 : ((a.x < b.x) ? -1 : 0);
@@ -151,21 +206,20 @@ function convertPdfToText(pdf) {
     for (let pageIndex = 0; pageIndex < pdf.formImage.Pages.length; pageIndex++) {
         let page = pdf.formImage.Pages[pageIndex];
         let smallestYValue = null;  // per page
-        let textsWithSameXvalues = {};
+        let textsWithSameXValues = {};
 
         for (let textIndex = 0; textIndex < page.Texts.length; textIndex++) {
             let text = page.Texts[textIndex];
-            if (!textsWithSameXvalues[text.x])
-                textsWithSameXvalues[text.x] = [];
-            textsWithSameXvalues[text.x].push(text);
+            if (!textsWithSameXValues[text.x])
+                textsWithSameXValues[text.x] = [];
+            textsWithSameXValues[text.x].push(text);
         }
 
         // Find smallest Y distance.
 
-        for (let x in textsWithSameXvalues) {
-            let texts = textsWithSameXvalues[x];
+        for (let x in textsWithSameXValues) {
+            let texts = textsWithSameXValues[x];
             for (let i = 0; i < texts.length; i++) {
-                let firstYvalue = texts[i].y;
                 for (let j = 0; j < texts.length; j++) {
                     if (texts[i] !== texts[j]) {
                         let distance = Math.abs(texts[j].y - texts[i].y);
@@ -207,16 +261,12 @@ function convertPdfToText(pdf) {
                 }
             };
 
+            // Create a new row and add the text to the row.
+
             if (!foundRow) {
-                // Create a new row.
-
                 let row = { y: text.y, data: [] };
-
-                // Add text to the row.
-
                 for (let index = 0; index < text.R.length; index++)
                     row.data.push({ text: decodeURIComponent(text.R[index].T), x: text.x });
-
                 rows.push(row);
             }
         };
@@ -258,7 +308,6 @@ function convertPdfToText(pdf) {
 
 function run(database) {
     parsePdfs(database, LodgedApplicationsUrl);
-    parsePdfs(database, ApprovedApplicationsUrl);
 }
 
 initializeDatabase(run);

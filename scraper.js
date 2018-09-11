6a7f10478b637cf667b54fc918ccb2d25d801c0d
MichaelBone <Michael.Bone@senet.com.au>
2018-09-11 19:03:35 +0930

    Added error event.

--- scraper.js
+++ scraper.js
@@ -102,6 +102,7 @@ function parsePdfs(database, url) {
 
             let pdfParser = new pdf2json();
             pdfParser.on("finish", () => { });
+            pdfParser.on("error", () => { });
             pdfParser.on("pdfParser_dataError", error => { console.error(error); });
             pdfParser.on("pdfParser_dataReady", pdf => {
                 // Convert the JSON representation of the PDF into a collection of PDF rows.
